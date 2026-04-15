5a91a62adbc7a7866f47dea88623e085c538c7a3
Alejandro <alejandro@example.com>
2026-04-14 20:21:28 -0500

    Remove static file serving - backend is API only

--- server.js
+++ server.js
@@ -18,12 +18,6 @@ let serialPortName = null;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-
-// Ruta principal
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 // -------- API: listar puertos --------
 app.get("/api/ports", async (req, res) => {
